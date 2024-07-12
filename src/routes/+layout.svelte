83058576099aa9c5b635e49d94456eae3723d22d
mjovanc <mjovanc@icloud.com>
2024-07-12 23:30:18 +0200

    Fixing stars animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,14 @@
 
   import { dev } from "$app/environment";
   import { inject } from "@vercel/analytics";
+  import StarryBackground from '$lib/components/StarryBackground.svelte';
 
   inject({ mode: dev ? "development" : "production" });
 </script>
 
+<StarryBackground />
 <Navbar />
+
 
 <div class="mx-auto my-10 w-full max-w-screen-xl p-4 py-6 lg:py-8">
   <slot />
